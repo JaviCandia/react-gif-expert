5aa5656bfc83f6187d090763f82e6c767f67be3d
Javier Candia <javi-candia@hotmail.com>
2024-02-19 01:57:42 -0600

    added a single comment

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { GifExpertApp } from "./GifExpertApp.jsx";
 import './styles.css';
 
+// Just a random comment
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GifExpertApp />
